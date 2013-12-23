56bd057c9b4f3ef829d2d02770c263e6d13337e8
Eben <eben.lowe@gmail.com>
2013-12-23 00:10:02 -0500

    Added dog model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131223045253) do
+ActiveRecord::Schema.define(version: 20131223050743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: true do |t|
+    t.string "name"
+  end
 
   create_table "owners", force: true do |t|
     t.string "firstname"
